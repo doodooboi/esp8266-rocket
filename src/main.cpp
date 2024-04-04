2d8f92ce28c9f7bdc57766f380e6d4ec9a8e45ad
David Tomaszewski <DT31883@student.leyden212.org>
2024-04-04 10:40:24 -0500

    Commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
 #include <Wire.h>
 
 #define PINS_LED1 D4
@@ -8,20 +9,34 @@
 #define GY521_ADDRESS 0x68
 
 const char* ssid = "NodeMCU AP";
-const char* password = "nodemcu";
+const char* password = "nodemcu1234";
 
-WiFiServer server(80);
+ESP8266WebServer server(80);
+
+IPAddress local_ip(192,168,1,1);
+IPAddress gateway(192,168,1,1);
+IPAddress subnet(255,255,255,0);
+
+void handle_NotFound(){
+  server.send(404, "text/plain", "Not found");
+}
+
+void handleRoot() {
+	server.send(200, "text/plain", "Connected");
+}
 
 void setup() {
   Serial.begin(115200);
 
-  Serial.print("Creating AP ");
-  boolean result = WiFi.softAP(ssid, password);
-  if (result == true) {
-    Serial.println("Ready");
-  } else {
-    Serial.println("Failed!");
-  }
+  WiFi.softAP(ssid, password);
+	WiFi.softAPConfig(local_ip, gateway, subnet);
+  delay(100);
+
+	server.on("/", handleRoot);
+	server.onNotFound(handle_NotFound);
+	server.begin();
 }
 
-void loop() {}
+void loop() {
+	server.handleClient();
+}
